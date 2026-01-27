fa5c569d5bffccecae5a7a7a7d672d4d81953159
Zachary Lam <32212567+Dapples0@users.noreply.github.com>
2026-01-27 15:55:07 +1100

    Removed comments

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     gb gb;
-    // 02-interrupts instr_timing interrupt_time dmg-acid2 cgb-acid2.gbc cpu_instrs
-    // 01-read_timing  02-write_timing 03-modify_timing
-    // mbctest MBC3_Test.gbc rtc3test
+
     gb.run((const char*)argv[1]);
 
 
